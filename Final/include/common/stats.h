99f9a8a1b888645d8d3625ce4d2e3df559ddd43b
D. Russell <dakota.mk.russell@gmail.com>
2022-06-27 15:10:43 -0600

    Initial commit of file structure.

---
+++ Final/include/common/stats.h
@@ -0,0 +1,111 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Calculate statistics for unsigned char data. 
+ *
+ * Provided an input array of unsigned char data, this file sorts the input array
+ * largest to smallest before calculating the following statistics:
+ * 1. minimum,
+ * 2. maximum, 
+ * 3. median,
+ * 4. mean.
+ * The statistic value returned is rounded down to the nearest integer.
+ *
+ * @author Dakota Russell
+ * @date 06/10/2022
+ *
+ */
+
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* @brief Find Maximum of Dataset.
+ *
+ * Find maximum value of dataset.
+ *
+ * @param array Input array to find maximum of.
+ * @param length Length of input array.
+ *
+ * @return Maximum value of dataset.
+ */
+unsigned char find_maximum(unsigned char* array, int length);
+
+/* @brief Find Mean of Dataset.
+ *
+ * Find mean value of dataset. Round mean down to nearest integer.
+ *
+ * @param array Input array to find mean of.
+ * @param length Length of input array.
+ *
+ * @return Mean value of dataset as integer.
+ */
+unsigned char find_mean(unsigned char* array, int length);
+
+/* @brief Find Median of Dataset.
+ *
+ * Find median value of dataset.
+ *
+ * @param array Input array to find median of.
+ * @param length Length of input array.
+ *
+ * @return Median value of dataset.
+ */
+unsigned char find_median(unsigned char* array, int length);
+
+/* @brief Find Minimum of Dataset.
+ *
+ * Find minimum value of dataset.
+ *
+ * @param array Input array to find minimum of.
+ * @param length Length of input array.
+ *
+ * @return Minimum value of dataset.
+ */
+unsigned char find_minimum(unsigned char* array, int length);
+
+/* @brief Print array.
+ *
+ * Print array values to screen.
+ *
+ * @param array Input array to print.
+ * @param length Length of input array.
+ *
+ * @return void.
+ */
+void print_array(unsigned char* array, int length);
+
+/* @brief Print statistics.
+ *
+ * Print provided statistics in a formatted manner to screen.
+ *
+ * @param minimum Dataset minimum as integer.
+ * @param maximum Dataset maximum as integer.
+ * @param median Dataset median as integer.
+ * @param mean Dataset mean as integer.
+ *
+ * @return void.
+ */
+void print_statistics(unsigned char minimum, unsigned char maximum, 
+		unsigned char median, unsigned char mean);
+
+/* @brief Sort array.
+ *
+ * Sort input array from largest to smallest in-place.
+ *
+ * @param array Input array to sort.
+ * @param length Length of input array.
+ *
+ * @return void.
+ */
+void sort_array(unsigned char* array, int length);
+
+#endif
---
+++ Final/src/stats.c
@@ -0,0 +1,151 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c 
+ * @brief Statistic calculator.
+ *
+ * Given input data array, sort largest to smallest before calculating the
+ * following statistics:
+ * 1. minimum
+ * 2. maximum
+ * 3. median
+ * 4. mean.
+ * Each returned statistic is rounded down to the nearest integer. Sorted array and
+ * statistics are printed in a formatted style.
+ *
+ * @author Dakota Russell
+ * @date 06/10/2022
+ *
+ */
+
+#include <stdio.h>
+#include "stats.h"
+
+static void swap(unsigned char* x, unsigned char* y);
+static int partition(unsigned char* array, int left, int right, 
+		unsigned char pivot);
+static void sort_array_helper(unsigned char* array, int left, int right);
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+  
+  
+  /* Other Variable Declarations Go Here */
+  sort_array(test, SIZE);
+  unsigned char minimum = find_minimum(test, SIZE);
+  unsigned char maximum = find_maximum(test, SIZE);
+  unsigned char median = find_median(test, SIZE);
+  unsigned char mean = find_mean(test, SIZE);
+
+  /* Statistics and Printing Functions Go Here */
+  print_array(test, SIZE);
+  print_statistics(minimum, maximum, median, mean);
+}
+
+/* Add other Implementation File Code Here */
+unsigned char find_maximum(unsigned char* array, int length){
+  if (array == NULL) return 0;
+  unsigned char maximum = array[0];
+  for (int i=0; i<length; i++){
+    if (array[i] > maximum)
+	  maximum = array[i];  
+  }
+  return maximum;
+}
+
+unsigned char find_mean(unsigned char* array, int length){
+  if (array == NULL) return 0;
+  unsigned int sum = 0;
+  for (int i=0; i<length; i++){
+    sum += array[i];
+  }
+  return (unsigned char)(sum/length);
+}
+
+unsigned char find_median(unsigned char* array, int length){
+  if (array == NULL) return 0;
+  // Cannot assume input array was sorted.
+  sort_array(array, length);
+  return array[length/2];
+}
+
+unsigned char find_minimum(unsigned char* array, int length){
+  if (array == NULL) return 0;
+  unsigned char minimum = array[0];
+  for (int i=0; i<length; i++){
+    if (array[i] < minimum)
+	  minimum = array[i];  
+  }
+  return minimum;
+}
+
+void print_array(unsigned char* array, int length){
+  if (array == NULL || length == 0) return;
+  for (int i=0; i<length-1; i++){
+    printf("%d, ", array[i]);
+  }
+  printf("%d\n", array[length-1]);
+  return;
+}
+
+void print_statistics(unsigned char minimum, unsigned char maximum, 
+		unsigned char median, unsigned char mean){
+  printf("Minimum:\t%d,\n", minimum);
+  printf("Maximum:\t%d,\n", maximum);
+  printf("Median:\t%d,\n", median);
+  printf("Mean:\t%d.\n", mean);
+  return;
+}
+
+static void swap(unsigned char* x, unsigned char* y){
+  unsigned char tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
+
+static int partition(unsigned char* array, int left, int right, 
+		unsigned char pivot){
+  while (left <= right){
+    while (array[left] > pivot){ left++; }
+    while (array[right] < pivot){ right--; }
+
+    if (left <= right){
+      swap(&array[left], &array[right]);
+      left++;
+      right--;
+    }
+  }
+  return left;
+}
+
+static void sort_array_helper(unsigned char* array, int left, int right){
+  if (left >= right) return;
+
+  int pivot = (left + right)/2;
+  int partitionIndex = partition(array, left, right, array[pivot]);
+  sort_array_helper(array, left, partitionIndex-1);
+  sort_array_helper(array, partitionIndex, right);
+  return;
+}
+
+void sort_array(unsigned char* array, int length){
+  if (array == NULL || length == 0) return;
+  sort_array_helper(array, 0, length-1);
+  return;
+}
