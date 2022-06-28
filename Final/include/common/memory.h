f8d7e00883dce3021ae217e07a6d4e6fbaefcd06
D. Russell <dakota.mk.russell@gmail.com>
2022-06-28 11:32:19 -0600

    Function declarations authored for memory.h.

--- Final/include/common/memory.h
+++ Final/include/common/memory.h
@@ -23,6 +23,98 @@
 #define __MEMORY_H__
 
 /**
+* @brief Move data from src to dest.
+*
+* Given a source pointer, a destination pointer, and a byte length,
+* move the byte length of data from the source pointer to the destination 
+* pointer.
+* 
+* @param src Pointer to source data array
+* @param dst Pointer to destination data array
+* @param length Length of data to move, in bytes.
+* 
+* @return Destination pointer.
+*/
+uint8_t* my_memmove(uint8_t* src, uint8_t* dst, size_t length);
+
+/**
+* @brief Copy data from src to dest.
+*
+* Given a source pointer, a destination pointer, and a byte length,
+* copy the byte length of data from the source pointer to the destination 
+* pointer.
+* 
+* @param src Pointer to source data array
+* @param dst Pointer to destination data array
+* @param length Length of data to copy, in bytes.
+* 
+* @return Destination pointer.
+*/
+uint8_t* my_memcopy(uint8_t* src, uint8_t* dst, size_t length);
+
+/**
+* @brief Set data in src to value.
+*
+* Given a source pointer, a byte length, and a set value,
+* set the byte length of data in the source pointer to the set value.
+* 
+* @param src Pointer to source data array
+* @param length Length of data to set, in bytes.
+* @param value Value to set memory to.
+* 
+* @return Source pointer.
+*/
+uint8_t* my_memset(uint8_t* src, size_t length, uint8_t value);
+
+/**
+* @brief Set data in src to 0.
+*
+* Given a source pointer and  a byte length,
+* set the byte length of data in the source pointer to 0.
+* 
+* @param src Pointer to source data array
+* @param length Length of data to set, in bytes.
+* 
+* @return Source pointer.
+*/
+uint8_t* my_memzero(uint8_t* src, size_t length);
+
+/**
+* @brief Reverse the order of bytes.
+*
+* Given a source pointer and  a byte length,
+* reverse the order of the byte length of data in the source pointe.
+* 
+* @param src Pointer to source data array
+* @param length Length of data to reverse order of, in bytes.
+* 
+* @return Source pointer.
+*/
+uint8_t* my_reverse(uint8_t* src, size_t length);
+
+/**
+* @brief Reserve this number of bytes.
+*
+* Given a byte length, reserve this number of bytes in dynamic memory.
+* 
+* @param length Length of data to reserve, in bytes.
+* 
+* @return If successful, pointer to memory created. Else, NULL.
+*/
+int32_t* reserve_words(size_t length);
+
+/**
+* @brief Free dynamic memory.
+*
+* Given a source pointer, free this dynamic memory.
+* 
+* @param src Pointer to source data array.
+* 
+* @return void.
+*/
+void free_words(int32_t* src);
+
+/**
  * @brief Sets a value of a data array 
  *
  * Given a pointer to a char data set, this will set a provided
